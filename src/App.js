6b5ad265fef1656261d3eef5d06d4236d7a1c0c6
Arnav Aggarwal <arnavaggrwl@gmail.com>
2020-06-23 12:38:22 -0700

    Strip character counter

--- src/App.js
+++ src/App.js
@@ -7,45 +7,10 @@ import {
 } from 'recoil';
 import './App.css';
 
-const textState = atom({ // piece of state
-  key: 'textState',
-  default: '',
-});
-
-const TextInput = () => {
-  const [text, setText] = useRecoilState(textState);
-  const onChange = event => setText(event.target.value);
-
-  return (
-    <div>
-      <input type="text" value={text} onChange={onChange} />
-      <br />
-      Echo: {text}
-    </div>
-  )
-};
-
-const charCountState = selector({ // transforms state
-  key: 'charCountState',
-  get: ({ get }) => get(textState).length,
-});
-
-const CharacterCount = () => {
-  const count = useRecoilValue(charCountState);
-  return <>CharacterCount: {count}</>;
-}
-
-const CharacterCounter = () => (
-  <div>
-    <TextInput />
-    <CharacterCount />
-  </div>
-);
-
 function App() {
   return (
     <div className="app-body">
-      <CharacterCounter />
+      <div>Hello</div>
     </div>
   );
 }
