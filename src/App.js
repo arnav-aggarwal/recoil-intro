d2e3f48403c1e871e1d7cf1ee65827f98215ca6f
Arnav Aggarwal <arnavaggrwl@gmail.com>
2020-06-23 14:03:40 -0700

    Basic todo list complete

--- src/App.js
+++ src/App.js
@@ -1,18 +1,112 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   atom,
   selector,
   useRecoilState,
   useRecoilValue,
+  useSetRecoilState,
 } from 'recoil';
 import './App.css';
+import { getByTestId } from '@testing-library/react';
 
-function App() {
+const todoListState = atom({
+  key: 'todoListState',
+  default: [],
+});
+
+let id = 0;
+function getId() {
+  return id++;
+}
+
+function TodoItemCreator() {
+  const [inputValue, setInputValue] = useState('');
+  const setTodoList = useSetRecoilState(todoListState);
+
+  const addItem = () => {
+    setTodoList(oldTodoList => [
+      ...oldTodoList,
+      {
+        id: getId(),
+        text: inputValue,
+        isComplete: false,
+      },
+    ]);
+
+    setInputValue('');
+  };
+
+  const onChange = event => setInputValue(event.target.value);
+
   return (
-    <div className="app-body">
-      <div>Hello</div>
+    <div>
+      <input type="text" value={inputValue} onChange={onChange} />
+      <button onClick={addItem}>Add</button>
     </div>
   );
 }
 
-export default App;
+function TodoItem({ item }) {
+  const [todoList, setTodoList] = useRecoilState(todoListState);
+  const index = todoList.findIndex(listItem => listItem === item);
+
+  const editItemText = event => {
+    const newList = [...todoList];
+    newList[index] = {
+      ...item,
+      text: event.target.value,
+    };
+
+    setTodoList(newList);
+  };
+
+  const toggleItemCompletion = event => {
+    const newList = [...todoList];
+    newList[index] = {
+      ...item,
+      isComplete: !item.isComplete,
+    };
+
+    setTodoList(newList);
+  };
+
+  const deleteItem = event => {
+    const newList = [...todoList.slice(0, index), ...todoList.slice(index + 1, todoList.length + 1)];
+    setTodoList(newList);
+  }
+
+  return (
+    <div>
+      <input type="text" value={item.text} onChange={editItemText} />
+      <input
+        type="checkbox"
+        checked={item.isComplete}
+        onChange={toggleItemCompletion}
+      />
+      <button onClick={deleteItem}>X</button>
+    </div>
+  );
+}
+
+function TodoList() {
+  const todoList = useRecoilValue(todoListState);
+
+  return (
+    <>
+      {/* <TodoListStats /> */}
+      {/* <TodoListilters /> */}
+      <TodoItemCreator />
+      {
+        todoList.map((item => <TodoItem key={item.id} item={item} />))
+      }
+    </>
+  )
+}
+
+export default function App() {
+  return (
+    <div className="app-body">
+      <TodoList />
+    </div>
+  );
+}
