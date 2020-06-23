f39e19060e768336cb31467e3db08722b9c11cca
Arnav Aggarwal <arnavaggrwl@gmail.com>
2020-06-23 12:33:22 -0700

    Implement character counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  RecoilRoot,
+  atom,
+  selector,
+  useRecoilState,
+  useRecoilValue,
+} from 'recoil';
 import './App.css';
+
+const textState = atom({ // piece of state
+  key: 'textState',
+  default: '',
+});
+
+const TextInput = () => {
+  const [text, setText] = useRecoilState(textState);
+  const onChange = event => setText(event.target.value);
+
+  return (
+    <div>
+      <input type="text" value={text} onChange={onChange} />
+      <br />
+      Echo: {text}
+    </div>
+  )
+};
+
+const charCountState = selector({ // transforms state
+  key: 'charCountState',
+  get: ({ get }) => get(textState).length,
+});
+
+const CharacterCount = () => {
+  const count = useRecoilValue(charCountState);
+  return <>CharacterCount: {count}</>;
+}
+
+const CharacterCounter = () => (
+  <div>
+    <TextInput />
+    <CharacterCount />
+  </div>
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RecoilRoot>
+      <div className="app-body">
+        <CharacterCounter />
+      </div>
+    </RecoilRoot>
   );
 }
 
